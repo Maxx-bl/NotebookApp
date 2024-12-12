975174276979c2116eac8e3128a5f68d9cbb305f
Maxx <max13.bl@gmail.com>
2024-12-12 22:58:44 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('Notebook'),
+        ),
+        ),
+      );
+  }
+}
